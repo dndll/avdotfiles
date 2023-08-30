0571a9796ca3c4e1b45705e03698d4038506eb18
Donovan Dall <awesomeibex@proton.me>
2023-08-30 22:24:18 +0100

    configs

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,11 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.motion.hop-nvim"},
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  { import = "astrocommunity.pack.python" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
