f0a4e5ab9c496040892af971cd946370f7773d25
Donovan Dall <awesomeibex@proton.me>
2023-09-04 19:00:25 +0100

    plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,10 +5,42 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+
   { import = "astrocommunity.motion.hop-nvim"},
+
+  { import = "astrocommunity.code-runner.sniprun" },
+  { import = "astrocommunity.color.twilight-nvim" },
+
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.cmp-cmdline"},
+
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim"},
+  { import = "astrocommunity.diagnostics.trouble-nvim"},
+
+  { import = "astrocommunity.editing-support.auto-save-nvim"},
+  { import = "astrocommunity.editing-support.comment-box-nvim"},
+  { import = "astrocommunity.editing-support.cutlass-nvim"},
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim"},
+  { import = "astrocommunity.editing-support.todo-comments-nvim"},
+  { import = "astrocommunity.editing-support.ultimate-autopair-nvim"},
+  { import = "astrocommunity.editing-support.vim-move"},
+
+
+  { import = "astrocommunity.indent.mini-indentscope"},
+
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.lsp.lsp-inlayhints-nvim" },
+
+  { import = "astrocommunity.syntax.vim-sandwich" },
+
+  { import = "astrocommunity.utility.noice-nvim" },
+
+  { import = "astrocommunity.workflow.hardtime-nvim" },
+
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
-  { import = "astrocommunity.pack.python" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
 }
